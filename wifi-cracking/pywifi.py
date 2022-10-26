8df8c57eba62641c8b633ecede8c5e98ac74d7bd
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-10-26 16:43:47 +0800

    added wifi-cracking files

---
+++ wifi-cracking/pywifi.py
@@ -0,0 +1,13 @@
+import time
+import pywifi
+
+wifi = pywifi.PyWiFi()
+iface = wifi.interfaces()[0]
+iface.scan()
+time.sleep(0.5)
+results = iface.scan_results()
+
+for i in results:
+    bssid = i.bssid
+    ssid  = i.ssid
+    print(f"{bssid}: {ssid}")
