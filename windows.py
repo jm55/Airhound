5137471c2c488ed0682ef450dd367a16a708cf19
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-10-27 23:23:09 +0800

    Added windows easter egg
    This is not the focus, since it is on Linux, but added as an easter egg feature.

--- windows.py
+++ windows.py
@@ -14,7 +14,7 @@ windows.py module
 
 import utils.utils as utils
 import subprocess
-
+import re
 
 def run():
     utils.cls()
@@ -26,3 +26,24 @@ def run():
     extract_wifi()
 
 def extract_wifi():
+    netsh = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+    profiles = re.findall("All User Profile     : (.*)\r", netsh)
+    wifi_list = []
+    if len(profiles) != 0:
+        for name in profiles:
+            wifi_profile = {}
+            profile = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+            if re.search("Security key           : Absent", profile):
+                continue
+            else:
+                wifi_profile["ssid"] = name
+                profile_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+                password = re.search("Key Content            : (.*)\r", profile_pass)
+                if password == None:
+                    wifi_profile["password"] = None
+                else:
+                    wifi_profile["password"] = password[1]
+                wifi_list.append(wifi_profile) 
+
+    for x in range(len(wifi_list)):
+        print(wifi_list[x]['ssid'] + " > " + wifi_list[x]['password'])
