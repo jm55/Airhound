e55f17aa2cdc8e282566db1880312c9e9659dd64
kali <71485171+jm55DLSU@users.noreply.github.com>
2022-10-29 12:19:50 +0800

    Updated file headers

--- scanning/interfaces/interfaces.py
+++ scanning/interfaces/interfaces.py
@@ -1,14 +1,20 @@
 '''
-==============================
-NSSECU2 - Hacking Tool Project
-==============================
-Members: Escalona, Fadrigo, Fortiz, Manzano, Sy
-Topic: WiFi Hacking Tool
-Description: The objective of this project is to create a packaged tool that will be able to do Wi-Fi scanning, cracking, and admin control access.
-Objective Functionalities:
-    1. WiFi Scanning & Handshake Capture
-    2. WiFi Cracking
-    3. WAP/Router Admin Control Access
+============================================================
+                NSSECU2 - Hacking Tool Project
+============================================================
+        Members: Escalona, Fadrigo, Fortiz, Manzano, Sy
+        Topic: WiFi Hacking Tool
+        Description: The objective of this project is to 
+                     create a packaged tool that will be 
+                     able to do Wi-Fi scanning, cracking, 
+                     and admin control access.
+        Objective Functionalities:
+            1. WiFi Scanning & Handshake Capture
+            2. WiFi Cracking
+            3. WAP/Router Admin Control Access
+            4. WiFi DOS
+            5. Windows Saved WiFi Passwords
+============================================================
 
 WLAN INTERFACE SEARCH MODULE
 '''
