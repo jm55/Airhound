50cc54cd54537abba1066a58d17cd697df653b3d
kali <71485171+jm55DLSU@users.noreply.github.com>
2022-11-04 12:28:43 +0800

    Finished .cap to .hccap/.hccapx converters

--- utils/converters.py
+++ utils/converters.py
@@ -27,17 +27,31 @@ def cap_to_HS():
     while True:
         utils.header("Utility Converter", ["Convert captured file to Hashcat Capture file",".cap to .hccap"])
         filename = input("Enter filename: ")
-        if filename.strip() == "":
+        if filename.strip() != "":
             break
     if ".cap" not in filename:
         filename += ".cap"
 
-    #ASK FOR OUTPUT FILENAME
-
-    #CHECK FILE
-
-    #DO AIRCRACK COMMAND: aircrack-ng -j hashcat w_handshake.cap
-    return None
+    hcap = "aircrack-ng -J hashcat " + filename
+    subprocess.Popen(hcap, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    
+    output_filename = filename[:len(filename)-4] + ".hccap"
+    subprocess.Popen("mv hashcat.hccap " + output_filename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    return output_filename
 
 def cap_to_HS3():
-    return None
+    filename = ""
+    while True:
+        utils.header("Utility Converter", ["Convert captured file to Hashcat Capture file",".cap to .hccapx"])
+        filename = input("Enter filename: ")
+        if filename.strip() != "":
+            break
+    if ".cap" not in filename:
+        filename += ".cap"
+
+    hcapx = "aircrack-ng -j hashcat " + filename
+    subprocess.Popen(hcapx, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    
+    output_filename = filename[:len(filename)-4] + ".hccapx"
+    subprocess.Popen("mv hashcat.hccapx " + output_filename, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).wait()
+    return output_filename
